7ad0f7d58c4f547a938f4445380e621533bdd3ad
skyline75489 <skyline75489@outlook.com>
2014-12-24 12:02:00 +0800

    NSURLConnection's API is broken for swift

--- swift-playground/main.swift
+++ swift-playground/main.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-println("Hello, World!")
+// Async
+class MyNSURLConnectionDelegate: NSObject, NSURLConnectionDataDelegate {
+    private var receivedData:NSData?
+    override init() {
+        receivedData = nil;
+    }
+    func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        receivedData = data
+        println(receivedData)
+    }
+}
 
+let url = NSURL(string: "http://httpbin.org/get")
+let req = NSURLRequest(URL: url!)
+var resp:AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
+var err:NSErrorPointer = nil
+
+let receivedData = NSURLConnection.sendSynchronousRequest(req, returningResponse: resp, error: err)
+
+let str = NSString(data: receivedData!, encoding: NSUTF8StringEncoding)
+println(str)
