03fe1f1edcf71631d584e626766e187490e9afd1
skyline75489 <skyline75489@outlook.com>
2014-12-24 11:21:49 +0800

    Initial Commit

---
+++ swift-playground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-playground
+//
+//  Created by skyline on 14/12/24.
+//  Copyright (c) 2014年 skyline. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
