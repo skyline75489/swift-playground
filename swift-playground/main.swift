0a01a135aff4ed2929b5a1abb9956d2745f9b712
skyline75489 <skyline75489@outlook.com>
2015-01-18 20:23:05 +0800

    add SwiftyJSON

--- swift-playground/main.swift
+++ swift-playground/main.swift
@@ -1,1 +1,9 @@
-println("hello")
+// GET Request
+
+let url = "http://httpbin.org/get"
+let para = ["Hello": "World"]
+
+if let resp = Requests.get(url, payload: para) {
+    let json = JSON(data: resp)
+    println(json)
+}
