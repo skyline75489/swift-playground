7ded5eeb76abcd5d5a738a908e1bdadf8e400400
skyline75489 <skyline75489@outlook.com>
2015-02-15 14:12:23 +0800

    add ExSwift

--- swift-playground/main.swift
+++ swift-playground/main.swift
@@ -1,28 +1,4 @@
 import Foundation
 
-
-func isValid(s:String) -> Bool {
-    let para_map = ["(":")",
-                    "{":"}",
-                    "[":"]"]
-    var stack = Stack<String>()
-    for ch in s {
-        let top = String(ch)
-        if (stack.count == 0) {
-            stack.push(top)
-            continue
-        }
-        if let p = para_map[stack.top()!] {
-            if (p == top) {
-                stack.pop()
-            } else {
-                stack.push(top)
-            }
-        } else {
-            stack.push(top)
-        }
-    }
-    return stack.count == 0
-}
-
-println(isValid("{}[()]"))
+let s = "www.github.com"
+println(s.contains("g"))
