951490859c620c478f9c1561ca5d37fcd8765999
skyline75489 <skyline75489@outlook.com>
2015-06-23 14:08:44 +0800

    string

--- swift-playground/main.swift
+++ swift-playground/main.swift
@@ -1,10 +1,53 @@
 import Foundation
 
-var j:JSON?
-
-if let json = j {
-    let a = json["Hello"]
+class Album {
+    var name: String
+    var artist: String
+    init(name: String, artist: String) {
+        self.name = name;
+        self.artist = artist;
+    }
 }
 
+extension Album: CustomDebugStringConvertible {
+    internal var debugDescription: String {
+        return "[\(artist): \(name)]"
+    }
+}
 
-print("Hell");
+extension Album: CustomStringConvertible {
+    var description: String {
+        return "\(artist) - \(name)"
+    }
+}
+
+var albums = [
+    Album(name: "A Thousand Suns", artist: "Linkin Park"),
+    Album(name: "Hybrid Theory", artist: "Linkin Park"),
+    Album(name: "V", artist: "Maroon 5"),
+    Album(name: "Songs About Janes", artist: "Maroon 5"),
+    Album(name: "My Everything", artist: "Ariana Grande"),
+    Album(name: "Yours Truly", artist: "Ariana Grande"),
+    Album(name: "Ocean Eyes", artist: "Owl City"),
+    Album(name: "Maybe I'm Dreaming", artist: "Owl City")
+]
+
+func getAlbumsOfArtist(list: [Album], artist: String) -> [Album] {
+    return albums.filter { album in
+        return album.artist == artist
+    }
+}
+
+func dumpJSON(list: [Album]) -> JSON {
+    let arr = albums.map { album in
+        return [
+            "artist": album.artist,
+            "name": album.name
+        ]
+    }
+    return JSON(arr);
+}
+
+let owlCity = getAlbumsOfArtist(albums, artist: "Linkin Park")
+print(owlCity);
+print(dumpJSON(albums))
