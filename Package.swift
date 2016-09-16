6395eefb5266fe5872a0f120d94c0e3c5614a87f
skyline75489 <skyline75489@outlook.com>
2016-09-16 18:19:16 +0800

    up

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "swift-playground",
-    dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
-    ]
-)
-
--- Sources/swift_playground.swift
+++
@@ -1,4 +0,0 @@
-struct swift_playground {
-
-    var text = "Hello, World!"
-}
