6abe2227a27b80cc3f83400022c340f35dcb6069
skyline75489 <skyline75489@outlook.com>
2016-09-16 18:40:59 +0800

    up

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,17 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import PlaygroundSupport
 
 var str = "Hello, playground"
 
 let a = str.substring(from: str.index(str.startIndex, offsetBy: 5))
 
 let b = str.substring(to: str.index(str.startIndex, offsetBy: 5))
+
+
+let hostView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 400))
+hostView.backgroundColor = UIColor.blue
+PlaygroundPage.current.liveView = hostView
+
+hostView
